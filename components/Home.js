05d9ba4ca51e7dafe03f1a8ee12d955d97c2883c
Frqnku <ugo.tiberto@gmail.com>
2025-10-01 17:34:08 +0200

    added consigne

--- components/Home.js
+++ components/Home.js
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import styles from '../styles/Home.module.css';
 import Dragon from './Dragon';
 
+// FETCH l'url suivant : https://dragon-back.vercel.app/dragons
+
 function Home() {
   const [dragon, setDragon] = useState(null);
 
